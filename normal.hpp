d4b6d8a4042f394078fbbd33776c6bb78d9b98d4
Tzu Chieh <james093131@gmail.com>
2021-06-26 01:17:23 +0800

    class

--- normal.hpp
+++ normal.hpp
@@ -1,4 +1,15 @@
-
+typedef vector<char> c1d;
+typedef vector<c1d> c2d;
+typedef vector<c2d> c3d;
+typedef vector<c3d> c4d;
+typedef vector<int> i1d;
+typedef vector<i1d> i2d;
+typedef vector<i2d> i3d;
+typedef vector<i3d> i4d;
+typedef vector<double>d1d;
+typedef vector<d1d> d2d;
+typedef vector<d2d> d3d;
+typedef vector<d3d> d4d;
 
 
 #include "CEC21.hpp"
